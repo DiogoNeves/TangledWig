48ef8c1a14bf21a46bbdb2c8286e3d68aeb77500
Diogo Neves <diogo.neves@gmail.com>
2023-02-28 11:22:52 +0000

    fixes wasm build

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.build.Builder) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = .{ .cpu_arch = .wasm32, .os_tag = .wasi };
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
